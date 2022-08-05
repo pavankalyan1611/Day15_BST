7fbd3ab38da505405ab1a80cd4c824a825900755
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-05 18:21:24 +0530

    [pavan] Creating BST

---
+++ Day15/Day15_BST/BinarySearchTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_BST
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData
+        {
+            get; set;
+        }
+        public BinarySearchTree<T> leftTree { get; set; }
+        public BinarySearchTree<T> rightTree { get; set; }
+
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.rightTree = null;
+            this.leftTree = null;
+
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Add(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0) // "A".CompareTo("a")
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new BinarySearchTree<T>(item);
+                }
+                else
+                    this.leftTree.Add(item);
+
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new BinarySearchTree<T>(item);
+                }
+                else
+                    this.rightTree.Add(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+        }
+    }
+}
---
+++ Day15/Day15_BST/Program.cs
@@ -0,0 +1,7 @@
+﻿using Day15_BST;
+
+
+BinarySearchTree<int> binary = new BinarySearchTree<int>(56);
+binary.Add(30);
+binary.Add(70);
+binary.Display();
